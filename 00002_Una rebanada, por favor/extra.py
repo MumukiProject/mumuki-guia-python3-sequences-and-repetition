9ead1a5c0de18d40706154d1e27e2683d99d2335
franco@mumuki.org <franco@mumuki.org>
2021-03-14 20:07:40 +0000

    Mumuki Export on 2021-03-14 20:07:40 +0000

--- 00002_Una rebanada, por favor/extra.py
+++ 00002_Una rebanada, por favor/extra.py
@@ -2,10 +2,11 @@ def listo():
   pass
 
 
-primera_estrofa = """Aquí me pongo a cantar		
+primera_estrofa = """
+Aquí me pongo a cantar		
 al compás de la vigüela,		
 que el hombre que lo desvela		
 una pena estraordinaria,		
 como la ave solitaria	
 con el cantar se consuela.
-"""
+""".strip()
