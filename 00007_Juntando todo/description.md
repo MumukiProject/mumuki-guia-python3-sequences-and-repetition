710f927dd846c3c125f3a18524d7e0febf010385
franco@mumuki.org <franco@mumuki.org>
2021-03-15 05:51:46 +0000

    Mumuki Export on 2021-03-15 05:51:46 +0000

--- 00007_Juntando todo/description.md
+++ 00007_Juntando todo/description.md
@@ -3,6 +3,10 @@
 > Definí la función `juntar` que tome por parámetro una lista de strings y los una de la siguiente forma: 
 > 
 > ```python
-> juntar(["super", "califragilistico", "espialidoso"])
-> supercalifragilisticoespialidoso
+> ム juntar(["super", "califragilistico", "espialidoso"])
+> "supercalifragilisticoespialidoso"
+> ム juntar(["cuatri", "motor"])
+> "cuatrimotor"
+> ム juntar([])
+> ""
 > ```
--- 00007_Juntando todo/test.py
+++ 00007_Juntando todo/test.py
@@ -1,4 +1,11 @@
 class Test(unittest.TestCase):
 
-  def test_description_example(self):
-    self.assertTrue(True)
+  def test_juntar_super_califragilistico_espialidoso_devuelve_supercalifragilisticoespialidoso(self):
+    self.assertEqual(juntar(["super", "califragilistico", "espialidoso"]), "supercalifragilisticoespialidoso")
+    
+  def test_juntar_lista_vacia_devuelve_string_vacio(self):
+    self.assertEqual(juntar([]), "")
+    
+  
+  def test_juntar_cuatri_motor_devuelve_cuatrimotor(self):
+    self.assertEqual(juntar(["cuatri", "motor"]), "cuatrimotor")
