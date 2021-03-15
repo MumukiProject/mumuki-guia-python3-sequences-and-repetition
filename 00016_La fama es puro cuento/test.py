0ea8f8bc0cbb46f4d8b2380481046d6dd8b0872d
franco@mumuki.org <franco@mumuki.org>
2021-03-15 08:50:39 +0000

    Mumuki Export on 2021-03-15 08:50:39 +0000

--- 00016_La fama es puro cuento/test.py
+++ 00016_La fama es puro cuento/test.py
@@ -1,4 +1,8 @@
 class Test(unittest.TestCase):
 
-  def test_description_example(self):
-    self.assertTrue(True)
+  def test_cuantas_veces_entreno_lo_suficiente_con_35_40_32_60_es_4(self):
+    self.assertEquals(cuantas_veces_entreno_lo_suficiente([35, 40, 32, 60]), 4)
+    
+  def test_cuantas_veces_entreno_lo_suficiente_con_15_45_90_0_es_2(self):
+    self.assertEquals(cuantas_veces_entreno_lo_suficiente([15, 45, 90, 0]), 2)
+    
