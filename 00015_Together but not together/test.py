a9393fc4436c74a99ed249943361e732bf0eba8b
franco@mumuki.org <franco@mumuki.org>
2023-03-29 19:01:22 +0000

    Mumuki Export on 2023-03-29 19:01:22 +0000

--- 00015_Together but not together/test.py
+++ 00015_Together but not together/test.py
@@ -1,18 +1,18 @@
 class Test(unittest.TestCase):
 
-  def test_juntar_super_califragilistico_espialidoso_devuelve_supercalifragilisticoespialidoso(self):
-    self.assertEqual(juntar("", ["super", "califragilistico", "espialidoso"]), "supercalifragilisticoespialidoso")
+  def test_join_strings_super_califragilistico_espialidoso_devuelve_supercalifragilisticoespialidoso(self):
+    self.assertEqual(join_strings("", ["super", "califragilistico", "espialidoso"]), "supercalifragilisticoespialidoso")
     
   
-  def test_juntar_cuatri_motor_devuelve_cuatrimotor(self):
-    self.assertEqual(juntar("", ["cuatri", "motor"]), "cuatrimotor")
+  def test_join_strings_cuatri_motor_devuelve_cuatrimotor(self):
+    self.assertEqual(join_strings("", ["cuatri", "motor"]), "cuatrimotor")
     
-  def test_juntar_hola_mundo_con_un_espacio_devuelve_hola_mundo(self):
-    self.assertEqual(juntar(" ", ["hola", "mundo"]), "hola mundo")
+  def test_join_strings_hola_mundo_con_un_espacio_devuelve_hola_mundo(self):
+    self.assertEqual(join_strings(" ", ["hola", "mundo"]), "hola mundo")
     
       
-  def test_juntar_hola_mundo_con_una_coma_devuelve_hola_coma_mundo(self):
-    self.assertEqual(juntar(",", ["hola", "mundo"]), "hola,mundo")
+  def test_join_strings_hola_mundo_con_una_coma_devuelve_hola_coma_mundo(self):
+    self.assertEqual(join_strings(",", ["hola", "mundo"]), "hola,mundo")
     
     
   
