715f63db7d40eab82d00be9329665583fd17b43c
franco@mumuki.org <franco@mumuki.org>
2023-04-04 19:10:42 +0000

    Mumuki Export on 2023-04-04 19:10:42 +0000

--- 00009_Loop until your dream come true/test.py
+++ 00009_Loop until your dream come true/test.py
@@ -1,8 +1,8 @@
 class Test(unittest.TestCase):
 
-  def test_todos_los_dias_un_poco_con_35_40_32_60_es_True(self):
-    self.assertTrue(todos_los_dias_un_poco([35, 40, 32, 60]))
+  def test_in_a_streak_with_35_40_32_60_equals_True(self):
+    self.assertTrue(in_a_streak([35, 40, 32, 60]))
     
-  def test_todos_los_dias_un_poco_con_15_45_90_0_es_False(self):
-    self.assertFalse(todos_los_dias_un_poco([15, 45, 90, 0]))
+  def test_in_a_streak_with_15_45_90_0_equals_False(self):
+    self.assertFalse(in_a_streak([15, 45, 90, 0]))
     
