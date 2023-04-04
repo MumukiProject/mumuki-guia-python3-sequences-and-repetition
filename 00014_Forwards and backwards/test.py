ed14e789611a8f3caf9170e9f9c8670c4d4745c1
franco@mumuki.org <franco@mumuki.org>
2023-04-04 19:35:48 +0000

    Mumuki Export on 2023-04-04 19:35:48 +0000

--- 00014_Forwards and backwards/test.py
+++ 00014_Forwards and backwards/test.py
@@ -1,22 +1,22 @@
 class Test(unittest.TestCase):
 
-  def test_sin_extremos_de_la_lista_4_5_10_2_3_devuelve_la_lista_5_10_2(self):
-    self.assertEquals(sin_extremos([4, 5, 10, 2, 3]), [5, 10, 2])
+  def test_internals_of_list_4_5_10_2_3_returns_list_5_10_2(self):
+    self.assertEquals(internals([4, 5, 10, 2, 3]), [5, 10, 2])
     
-  def test_sin_extremos_de_la_lista_4_5_10_2_3_9_devuelve_la_lista_5_10_2_9(self):
-    self.assertEquals(sin_extremos([4, 5, 10, 2, 3, 9]), [5, 10, 2, 3])
+  def test_internals_of_list_4_5_10_2_3_9_returns_list_5_10_2_9(self):
+    self.assertEquals(internals([4, 5, 10, 2, 3, 9]), [5, 10, 2, 3])
     
-  def test_sin_extremos_de_la_lista_4_5_10_devuelve_la_lista_5(self):
-    self.assertEquals(sin_extremos([4, 5, 10]), [5])
+  def test_internals_of_list_4_5_10_returns_list_5(self):
+    self.assertEquals(internals([4, 5, 10]), [5])
         
-  def test_sin_extremos_de_la_lista_40_20_devuelve_la_lista_vacia(self):
-    self.assertEquals(sin_extremos([40, 20]), [])
+  def test_internals_of_list_40_20_returns_list_vacia(self):
+    self.assertEquals(internals([40, 20]), [])
     
-  def test_extremos_de_la_lista_4_5_10_2_3_devuelve_la_lista_4_3(self):
-    self.assertEquals(extremos([4, 5, 10, 2, 3]), [4, 3])
+  def test_extremes_of_list_4_5_10_2_3_returns_list_4_3(self):
+    self.assertEquals(extremes([4, 5, 10, 2, 3]), [4, 3])
     
-  def test_extremos_de_la_lista_4_3_devuelve_la_lista_4_3(self):
-    self.assertEquals(extremos([4, 3]), [4, 3])
+  def test_extremes_of_list_4_3_returns_list_4_3(self):
+    self.assertEquals(extremes([4, 3]), [4, 3])
     
-  def test_extremos_de_la_lista_1_2_5_devuelve_la_lista_1_5(self):
-    self.assertEquals(extremos([1, 2, 5]), [1, 5])      
+  def test_extremes_of_list_1_2_5_returns_list_1_5(self):
+    self.assertEquals(extremes([1, 2, 5]), [1, 5])      
