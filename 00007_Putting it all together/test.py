41cecb888687c1227455c33030b22ba11852f75c
franco@mumuki.org <franco@mumuki.org>
2023-04-04 18:36:40 +0000

    Mumuki Export on 2023-04-04 18:36:40 +0000

--- 00007_Putting it all together/test.py
+++ 00007_Putting it all together/test.py
@@ -1,11 +1,13 @@
 class Test(unittest.TestCase):
 
-  def test_juntar_super_califragilistico_espialidoso_devuelve_supercalifragilisticoespialidoso(self):
-    self.assertEqual(juntar(["super", "califragilistico", "espialidoso"]), "supercalifragilisticoespialidoso")
+  def test_join_strings_super_califragilistic_expialidocious_returns_supercalifragilisticexpialidocious(self):
+    self.assertEqual(join_strings(["super", "califragilistic", "expialidocious"]), "supercalifragilisticexpialidocious")
     
-  def test_juntar_lista_vacia_devuelve_string_vacio(self):
-    self.assertEqual(juntar([]), "")
-    
-  
-  def test_juntar_cuatri_motor_devuelve_cuatrimotor(self):
-    self.assertEqual(juntar(["cuatri", "motor"]), "cuatrimotor")
+  def test_join_strings_empty_list_returns_empty_string(self):
+    self.assertEqual(join_strings([]), "")
+      
+  def test_join_strings_butter_fly_returns_butterfly(self):
+    self.assertEqual(join_strings(["butter", "fly"]), "butterfly")
+
+  def test_join_strings_what_s_it_returns_whatsit(self):
+    self.assertEqual(join_strings(["what",  "s", "it"]), "whatsit")
