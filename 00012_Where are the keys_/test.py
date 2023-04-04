6d15388e39bde4746dd6403dacc9ba84cde75b95
franco@mumuki.org <franco@mumuki.org>
2023-04-04 19:23:07 +0000

    Mumuki Export on 2023-04-04 19:23:07 +0000

--- 00012_Where are the keys_/test.py
+++ 00012_Where are the keys_/test.py
@@ -1,7 +1,13 @@
 class Test(unittest.TestCase):
 
-  def test_las_llaves_estan_en_primer_lugar(self):
-    self.assertEquals(donde_estan_las_llaves("🔑🔥👓"), 1)
+  def test_keys_are_in_first_place(self):
+    self.assertEquals(where_are_the_keys("🔑🔥👓"), 1, "where_are_the_keys('🔑🔥👓') should be 1")
     
-  def test_las_llaves_estan_en_segundo_lugar(self):
-    self.assertEquals(donde_estan_las_llaves("👓🔑🔥"), 2)
+  def test_keys_are_in_second_place(self):
+    self.assertEquals(where_are_the_keys("👓🔑🔥"), 2, "where_are_the_keys('👓🔑🔥') should be 2")
+
+  def test_keys_are_in_third_place(self):
+    self.assertEquals(where_are_the_keys("🍪🧀🔑"), 3, "where_are_the_keys('🍪🧀🔑') should be 3")
+
+  def test_keys_are_in_fourth_place_with_duplicated_emojis(self):
+    self.assertEquals(where_are_the_keys("🍪🍪🍪🔑"), 4, "where_are_the_keys('🍪🍪🍪🔑') should be 4")        
