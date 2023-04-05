afef0422178b59076af68d5ed6235878c3618938
franco@mumuki.org <franco@mumuki.org>
2023-04-05 18:09:46 +0000

    Mumuki Export on 2023-04-05 18:09:46 +0000

--- 00016_Let's kick the beat/test.py
+++ 00016_Let's kick the beat/test.py
@@ -6,3 +6,9 @@ class Test(unittest.TestCase):
   def test_how_many_times_trained_enough_with_15_45_90_0_equals_2(self):
     self.assertEquals(how_many_times_trained_enough([15, 45, 90, 0]), 2)
 
+  def test_how_many_times_trained_enough_with_15_45_90_equals_1(self):
+    self.assertEquals(how_many_times_trained_enough([15, 45, 90]), 1)
+    
+  def test_how_many_times_trained_enough_with_42_45_43_equals_1(self):
+    self.assertEquals(how_many_times_trained_enough([42, 45, 43]), 1)    
+
